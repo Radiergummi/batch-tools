0bf6189e091fe68368641c0442ed8f63a69c682c
Mo <Radiergummi@users.noreply.github.com>
2015-03-30 18:46:14 +0200

    Create checkForAdministrativePermissions.cmd

---
+++ checkForAdministrativePermissions.cmd
@@ -0,0 +1,16 @@
+:: This checks wether the user the shell is running under has administrative privileges.
+::
+:: Example:
+:: call :checkadmin
+:: if errorlevel 1 (
+::    echo This script must be run as administrator.
+::    exit
+:: )
+
+:checkadmin
+net session >nul 2>&1
+if errorlevel 1 (
+	exit /B 1
+) else (
+	exit /B 0
+)
