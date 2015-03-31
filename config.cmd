7ddea84a1f3ba14749c6cbab3d277d71f3491b9e
Mo <Radiergummi@users.noreply.github.com>
2015-03-31 10:30:37 +0200

    Update and rename getValueFromIniFile.cmd to config.cmd

---
+++ config.cmd
@@ -0,0 +1,27 @@
+:: Sets a variable by fetching the value of a given key.
+:: Comments and sections will be ignored, so you can store your configuration in neatly organized .ini-files.
+:: Attention: The quotation marks (") around the key are important!
+::
+:: Parameters:
+:: %1: The ini file to search in
+:: %2: The key to look for
+:: %3 (optional): The name of the variable to set the value in
+::
+:: Example: 
+:: call :config settings.ini "key"    :: %key% will be value
+
+:config
+	set configfile=%~1
+	:: You could also provide a hardcoded filename here instead of using the first parameter.
+	:: To do so, swap the two "%~2" below with "%~1".
+	if not [%3] == [] ( 
+		set variable=%3
+	)
+
+	for /F "eol=; eol=[ tokens=1,2* delims==" %%i in ('findstr /b /l /i %~2= "%~dp0%configfile%"') do (
+		if %errorlevel% == 1 (
+			exit /B 1
+		)
+		set %variable%=%%~j
+	)
+exit /B 0
